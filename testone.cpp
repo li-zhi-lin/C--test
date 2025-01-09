906079294a8635d7d33b40479c5a4dcc4e2cd2dd
李玉辉 <3220886048@qq.com>
2025-01-09 14:05:37 +0800

    添加了一个新的avg

--- testone.cpp
+++ testone.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int add(int a, int b) {
     return a + b;
 }
+int avg(int a,int b){
+    return a+b/2
+}
+
 int main(){
     cout << "The sum is: " << add(5, 10) << endl; // 输出 5 和 10 的和
+    cout << "第二个平均数为" << avg(5,10)  << endl;//输出5和10的平均数
     return 0;
 }
