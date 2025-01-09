b50249c172865f1c9ddc2fd543b8bee170efda2b
李玉辉 <3220886048@qq.com>
2025-01-09 13:56:43 +0800

    添加了一个新的函数

--- testone.cpp
+++ testone.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// 第一版新增的函数：求和
+int add(int a, int b) {
+    return a + b;
+}
 int main(){
-    cout<<"hello" << endl;
+    cout << "The sum is: " << add(5, 10) << endl; // 输出 5 和 10 的和
     return 0;
 }
